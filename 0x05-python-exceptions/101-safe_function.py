33bf35c8bab16b4e21e179db050e9f0c4d65b975
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-03-28 15:46:35 -0700

    Task 08

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -17,7 +17,7 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        result = None
+    return (result)
