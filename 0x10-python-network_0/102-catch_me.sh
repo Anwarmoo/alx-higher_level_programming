bee35cd825de1c94315fae15c0d0d4cd7d110601
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-06-30 13:03:20 -0700

    0x10. Python - Network #0 #advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+#Makes a request to 0.0.0.0:5000/catch_me to get it!
+curl -sLX PUT --data "user_id=98" --header "origin:You got me!" 0.0.0.0:5000/catch_me
