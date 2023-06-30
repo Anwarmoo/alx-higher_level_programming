e6f2ddb92e00c674609c016db35b1ce966d75f4b
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-06-30 13:22:41 -0700

    0x10. Python - Network #0 #advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl POST request
-curl -sLX PUT -d "user_id=98" -H "Origin:You got me!" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
